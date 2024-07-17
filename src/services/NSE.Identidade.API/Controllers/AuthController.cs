7075dd85063c64d3adf6f048ae0263f3d978506e
Josue <pedrojosuediogo@gmail.com>
2024-07-17 14:36:17 -0700

    NSE.Identidade.API - creating controllers for login and register

---
+++ src/services/NSE.Identidade.API/Controllers/AuthController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NSE.Identidade.API.Controllers;
+
+[Route("api/identidade")]
+public class AuthController : MainController
+{
+
+}
---
+++ src/services/NSE.Identidade.API/Controllers/MainController.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NSE.Identidade.API.Controllers;
+
+[ApiController]
+public class MainController : Controller
+{
+}
