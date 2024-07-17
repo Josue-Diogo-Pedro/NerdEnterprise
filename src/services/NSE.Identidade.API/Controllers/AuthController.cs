bed323cab1de23ca4a32502918c7e0bd554d242c
Josue <pedrojosuediogo@gmail.com>
2024-07-17 14:41:20 -0700

    NSE.Identidade.API - change place for implementation of maincontroller

--- src/services/NSE.Identidade.API/Controllers/AuthController.cs
+++ src/services/NSE.Identidade.API/Controllers/AuthController.cs
@@ -6,29 +6,5 @@ namespace NSE.Identidade.API.Controllers;
 [Route("api/identidade")]
 public class AuthController : MainController
 {
-    protected ICollection<string> Errors = new List<string>();
-
-    protected ActionResult CustomResponse(object result = null)
-    {
-        if (OperacaoValida()) return Ok(result);
-
-        return BadRequest(new ValidationProblemDetails(new Dictionary<string, string[]>
-        {
-            {"Mensagens", Errors.ToArray() }
-        }));
-    }
-
-    protected ActionResult CustomResponse(ModelStateDictionary modelState)
-    {
-        var erros = modelState.Values.SelectMany(e => e.Errors).ToList();
-        erros.ForEach(erro => AdicionarErroProcessamento(erro.ErrorMessage));
-
-        return CustomResponse();
-    }
-
-    protected bool OperacaoValida() => !Errors.Any();
-
-    protected void AdicionarErroProcessamento(string erro) => Errors.Add(erro);
-
-    protected void LimparErrosProcessamento() => Errors.Clear();
+    
 }
--- src/services/NSE.Identidade.API/Controllers/MainController.cs
+++ src/services/NSE.Identidade.API/Controllers/MainController.cs
@@ -1,8 +1,34 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace NSE.Identidade.API.Controllers;
 
 [ApiController]
 public class MainController : Controller
 {
+    protected ICollection<string> Errors = new List<string>();
+
+    protected ActionResult CustomResponse(object result = null)
+    {
+        if (OperacaoValida()) return Ok(result);
+
+        return BadRequest(new ValidationProblemDetails(new Dictionary<string, string[]>
+        {
+            {"Mensagens", Errors.ToArray() }
+        }));
+    }
+
+    protected ActionResult CustomResponse(ModelStateDictionary modelState)
+    {
+        var erros = modelState.Values.SelectMany(e => e.Errors).ToList();
+        erros.ForEach(erro => AdicionarErroProcessamento(erro.ErrorMessage));
+
+        return CustomResponse();
+    }
+
+    protected bool OperacaoValida() => !Errors.Any();
+
+    protected void AdicionarErroProcessamento(string erro) => Errors.Add(erro);
+
+    protected void LimparErrosProcessamento() => Errors.Clear();
 }
