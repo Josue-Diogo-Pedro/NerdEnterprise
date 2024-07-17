076f6917228d9a8d19bd06c5a168084da7dadde4
Josue <pedrojosuediogo@gmail.com>
2024-07-17 14:39:34 -0700

    NSE.Identidade.API - creating model for settings values about jwt

---
+++ src/services/NSE.Identidade.API/Extensions/AppSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace NSE.Identidade.API.Extensions;
+
+public class AppSettings
+{
+    public string Secret { get; set; } = string.Empty;
+    public int ExpiracaoHoras { get; set; }
+    public string Emissor { get; set; } = string.Empty;
+    public string ValidoEm { get; set; } = string.Empty;
+}
