5395a997a37267ace3fcd7ad59effce06213db1d
Josue <pedrojosuediogo@gmail.com>
2024-07-17 14:18:56 -0700

    NSE.Identidade.API - add configurations to Context ro prepare migrations

---
+++ src/services/NSE.Identidade.API/Data/IdentidadeDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NSE.Identidade.API.Data;
+
+public class IdentidadeDbContext : IdentityDbContext
+{
+    public IdentidadeDbContext(DbContextOptions<IdentidadeDbContext> options) : base(options) { }
+}
