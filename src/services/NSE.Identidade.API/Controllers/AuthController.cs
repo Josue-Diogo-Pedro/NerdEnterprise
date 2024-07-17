b6e4bcb6222ab50e31e25ddfb55aea39dfa73b41
Josue <pedrojosuediogo@gmail.com>
2024-07-17 14:42:24 -0700

    NSE.Identidade.API - part of implementation of authcontroller

--- src/services/NSE.Identidade.API/Controllers/AuthController.cs
+++ src/services/NSE.Identidade.API/Controllers/AuthController.cs
@@ -1,10 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Options;
+using NSE.Identidade.API.Extensions;
 
 namespace NSE.Identidade.API.Controllers;
 
 [Route("api/identidade")]
 public class AuthController : MainController
 {
-    
+    private readonly SignInManager<IdentityUser> _signInManager;
+    private readonly UserManager<IdentityUser> _userManager;
+    private readonly AppSettings _appSettings;
+
+    public AuthController(SignInManager<IdentityUser> signInManager,
+                          UserManager<IdentityUser> userManager,
+                          IOptions<AppSettings> appSettings)
+    {
+        _signInManager = signInManager;
+        _userManager = userManager;
+        _appSettings = appSettings.Value;
+    }
+
+
+
+    private static long ToUnixEpochDate(DateTime date)
+        => (long)Math.Round((date.ToUniversalTime() - new DateTimeOffset(1970, 1, 1, 0, 0, 0, TimeSpan.Zero)).TotalSeconds);
 }
