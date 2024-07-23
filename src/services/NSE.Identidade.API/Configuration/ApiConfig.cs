d1cee69b8ad95d10ac0169df215b9dc00db9be8a
Josue <pedrojosuediogo@gmail.com>
2024-07-23 10:30:07 -0700

    NSE.Identidade.API - refactoring, create extension methods for ApiConfig

---
+++ src/services/NSE.Identidade.API/Configuration/ApiConfig.cs
@@ -0,0 +1,27 @@
+﻿namespace NSE.Identidade.API.Configuration;
+
+public static class ApiConfig
+{
+    public static IServiceCollection AddApiConfiguration(this IServiceCollection services) 
+    {
+        services.AddControllers();
+
+        return services;
+    }
+
+    public static IApplicationBuilder UseApiConfiguration(this IApplicationBuilder app, IWebHostEnvironment env) 
+    {
+        if (env.IsDevelopment())
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+
+        app.UseIdentityConfiguration();
+
+        return app;
+    }
+}
