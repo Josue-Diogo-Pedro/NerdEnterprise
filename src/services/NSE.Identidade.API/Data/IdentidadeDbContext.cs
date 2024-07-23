b5977840db51e030ba5a02740972da1450b4eb5f
Josue <pedrojosuediogo@gmail.com>
2024-07-23 07:23:10 -0700

    NSE.Identidade.API - solve errors and add class that will traduce error from english to portuguese

--- src/services/NSE.Identidade.API/Data/IdentidadeDbContext.cs
+++ src/services/NSE.Identidade.API/Data/IdentidadeDbContext.cs
@@ -5,5 +5,14 @@ namespace NSE.Identidade.API.Data;
 
 public class IdentidadeDbContext : IdentityDbContext
 {
+    //public IdentidadeDbContext()
+    //{
+        
+    //}
     public IdentidadeDbContext(DbContextOptions<IdentidadeDbContext> options) : base(options) { }
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //{
+    //    optionsBuilder.UseSqlServer("Data Source=DESKTOP-3OMCEOO\\SQLEXPRESS;Initial Catalog=EnterpriseDb;Integrated Security=True; Trust Server Certificate=true;MultipleActiveResultSets=true");
+    //    base.OnConfiguring(optionsBuilder);
+    //}
 }
