8143025afa1fa81f169e2a09794fd2e9c3c34927
Josue <pedrojosuediogo@gmail.com>
2024-07-23 21:24:29 -0700

    NSE.WebApp.MVC - finish to implement basics extensions methods in Program class

--- src/web/NSE.WebApp.MVC/Program.cs
+++ src/web/NSE.WebApp.MVC/Program.cs
@@ -3,32 +3,10 @@ using NSE.WebApp.MVC.Configuration;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddIdentityConfiguration();
-
 builder.Services.AddMvcConfiguration(builder.Configuration);
 
 var app = builder.Build();
 
-app.UseIdentityConfiguration();
-
-
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.UseMvcConfiguration(app.Environment);
 
 app.Run();
