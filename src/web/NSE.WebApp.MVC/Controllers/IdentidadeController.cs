10df25716897164d3c68dc997ff96cf8087017ff
Josue <pedrojosuediogo@gmail.com>
2024-07-25 10:37:06 -0700

    NSE.WebApp.MVC - add skelletons of Post action of Registro

--- src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
+++ src/web/NSE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -1,13 +1,20 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
 
 namespace NSE.WebApp.MVC.Controllers;
 
 public class IdentidadeController : Controller
 {
     [HttpGet("nova-conta")]
-    public IActionResult Registro() 
+    public IActionResult Registro()
     {
         return View();
+    }
+
+    [HttpPost("nova-conta")]
+    public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro) 
+    {
+
     }
 
 
